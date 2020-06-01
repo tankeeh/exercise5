35933730efe32fb038a8f56f72b7c642da8037b6
tankee <tankextreme@gmail.com>
2020-06-01 23:26:16 +0200

    have to solve Predecessor.dè

--- bst/bst.cpp
+++ bst/bst.cpp
@@ -472,7 +472,7 @@ namespace lasd {
         if(currnode != nullptr && currnode->HasLeftChild())
             temp = new Data(SubtreeMax(currnode->Left()));
 
-        std::cout<<"prova";
+        std::cout<<"provaa";
         if(temp != nullptr)
             return *temp;
         else throw std::length_error("Non e' presente un predecessore per questo elemento.");
